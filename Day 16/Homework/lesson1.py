4978e3846a9f634fcd841c1cae7c3be7638b0560
სანან მამედოვი <mamedovisanan57@gmail.com>
2024-09-01 17:10:39 +0400

    Delete Day 16 directory

--- Day 16/Homework/lesson1.py
+++
@@ -1,7 +0,0 @@
-#გავაკეთე 5 მაგალითი while ციკლზე
-numb = 20
-i = 0
-while i < int(numb):
-    print("sanan mamedov")
-    i = i + 6
-    
--- Day 16/Homework/lesson3.py
+++
@@ -1,5 +0,0 @@
-#გავაკეთე 5 მაგალითი while ციკლზე
-number = int(input("enter 1-1000: "))
-while number  != 32:
-    number = int(input("enter number : "))
-    
