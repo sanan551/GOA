079cd0c8cf753a0a6ddf1d5061c626569eb79379
სანან მამედოვი <mamedovisanan57@gmail.com>
2024-09-19 17:05:02 +0400

    Update lesson12.py

--- Goa/Day 25/Homework/lesson12.py
+++ Goa/Day 25/Homework/lesson12.py
@@ -1,4 +1,8 @@
-i = 10
-while i < 101:
-    print(i)
-    i = i +1
+number = 1
+
+while number <= 100:
+    if 50 <= number <= 60:
+        number += 1
+        continue
+    print(number)
+    number += 1
