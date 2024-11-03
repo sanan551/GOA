676bdf4a2d1f82d45c98b97ffee19951a4861a11
სანან მამედოვი <mamedovisanan57@gmail.com>
2024-11-03 18:34:15 +0400

    Add files via upload

---
+++ Goa/Day 39/Homework/lesson5.py
@@ -0,0 +1,13 @@
+def lists(list):
+    print(max(list))
+    print(min(list))
+    print(len(list))
+    print(sum(list))
+
+
+
+list = int(input('შეიყვანეთ რომელიმე რიცხვი: ')),int(input('შეიყვანეთ რომელიმე რიცხვები: ')),int(input('შეიყვანეთ რომელიმე რიცხვები: ')),int(input('შეიყვანეთ რომელიმე რიცხვები: '))
+result = lists(list)
+print(result)
+
+
